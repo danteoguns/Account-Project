d8062ad524584b0cd8bb9c112a4df87f10feb7c1
danteoguns <danteoguns@gmail.com>
2018-08-08 10:04:20 +0100

    Account Features

--- src/AccApp/App.java
+++ src/AccApp/App.java
@@ -1,10 +1,22 @@
 package AccApp;
 
+import AccApp.AccMgmt.*;
+
 public class App {
-
+	
+	static Service service = new Service();
+	static Account account;
+	
 	public static void main(String[] args) {
-		System.out.println("Hello world to the standard out");
-
+//		System.out.println("Hello world to the standard out");
+		
+		service.addAccount("1", new Account("Daniel", "Oguns", "12345"));
+		
+		Account account = service.getAccount("1");
+		
+		System.out.println("First Name is: " + account.getFirstName());
+		System.out.println("Last Name is: " + account.getLastName());
+		System.out.println("Account Number is: " + account.getAccountNumber());
 	}
-
+	
 }
